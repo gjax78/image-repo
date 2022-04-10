4659dc7cc27af8097a14d09388ddd2306b88f701
Geena Jackson <gjacks303@gmail.com>
2022-04-10 13:44:18 -0600

    Create like button

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ImageCard.css'
 
 const ImageCard = ({ src }) => {
+  const [isFav, setIsFav] = useState(false)
+
   return (
     <div className='card'>
       <img src={src} />
+      <button className='like-button'
+        onClick={() => setIsFav(!isFav)}
+        >{isFav ? '🤍' : '❤️'}</button>
     </div>
   )
 }
