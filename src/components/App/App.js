9c199c2368e818a8ef058bce6403955f6062afa5
Geena Jackson <gjacks303@gmail.com>
2022-04-12 07:46:34 -0600

    Add search bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import { getAllImages } from '../../apiCalls.js'
 import React, { useState, useEffect } from 'react'
 import Images from '../Images/Images'
+import Search from '../Search/Search'
 
 const App = () => {
   const [images, setImages] = useState([])
+  const [search, setSearch] = useState('')
 
   const getData = async () => {
     try {
@@ -17,14 +19,27 @@ const App = () => {
     }
   }
 
-  useEffect(() => {
-    getData()
-  }, [])
+  // useEffect(() => {
+  //   getData()
+  // }, [])
 
+  // const filteredSearch = () => {
+  //
+  // }
+
+  useEffect(() => {
+    if (search === null) {
+      getData()
+    } else if (search){
+      getData(images.filter(image => image.title.toLowerCase().includes(search)))
+    }
+    getData()
+  }, [search])
 
   return (
     <div className="App">
       <h1>Studio Ghibli Image Repository</h1>
+      <Search getQuery={(e) => setSearch(e)}/>
       <Images images={images} />
     </div>
   )
--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -3,15 +3,13 @@ import ImageCard from '../ImageCard/ImageCard'
 import './Images.css'
 
 const Images = ({ images }) => {
-  {console.log(images)}
   const imageCards = images.map(image => {
     return (
-      <div className='ideas-container'>
+      <div className='ideas-container' key={image.id}>
         <ImageCard
           src={image.image}
           title={image.title}
           originalTitle={image.original_title}
-          key={image.id}
         />
       </div>
     )
